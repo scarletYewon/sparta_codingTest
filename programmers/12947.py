ffd06d5f3922f6a1d1c55b3c73769853767d73b5
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-08-03 20:59:44 +0900

    Create 12947.py

---
+++ programmers/12947.py
@@ -0,0 +1,10 @@
+def solution(x):
+    X = list(str(x))
+    sum = 0
+    for i in range(len(X)):
+        sum += int(X[i])
+    if x%sum ==0:
+        return True
+    return False
+
+print(solution(10))
