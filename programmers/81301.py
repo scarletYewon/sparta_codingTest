7930fcb4dece3a76e5648f190f93c0b0bd887b58
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-08-04 00:49:00 +0900

    81301

---
+++ programmers/81301.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+  nums = ['zero','one','two','three','four','five', 'six','seven','eight','nine']
+  for i,n in enumerate(nums):
+    numbers = numbers.replace(n, str(i))
+  answer = int(numbers)
+  return answer
